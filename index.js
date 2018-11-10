b4b88422882b5fd5b8ab340e060e5de119852d1d
Aceheliflyer <aceheliflyer@gmail.com>
2018-11-10 14:52:13 -0500

    Create base of bot.

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
-// Matthew: Automated Server Bot
-var Commando = require('discord.js-commando')
-var path = require('path')
-var clientInfo = require('./clientinfo.json')
-var newMsg = '0'; var oof = 1
+(async () => {
+  process.chdir(__dirname)
+  const Discord = require('discord.js')
+  const client = new Discord.Client()
+  const config = require('./config.json')
+  const sqlite = require('sqlite')
+  const db = await sqlite.open('./database.sqlite')
 
-let client = new Commando.CommandoClient({
-  disableEveryone: true,
-  owner: '203587309843513344',
-  commandPrefix: 'm/'
-})
+  client.on('ready', () => {
+    console.log('I am ready!')
+  })
 
-client.registry.registerDefaults()
-client.registry.registerCommandsIn(path.join(__dirname, 'commands'))
+  client.on('message', message => {
+    if (message.author.bot) return
+    if (message.content.toLowerCase() === 'ping') message.channel.send('Pong!')
+  })
 
-client.on('ready', () => {
-  console.log('Bot Rebooted')
-})
-
-client.on('message', msg => {
-  if (msg.author.bot) return
-  if (!newMsg) {
-    newMsg = msg.content
-    if (!msg.channel.topic) {
-      msg.channel.setTopic(newMsg)
-    }
-    console.log(msg.content)
-  } else {
-    if (parseInt(msg.content) !== (parseInt(msg.channel.topic) + 1)) {
-      return msg.delete() && msg.author.send('Please count up by 1.')
-    } else {
-      console.log(msg.content)
-      oof++
-      msg.channel.setTopic(oof - 1)
-      newMsg = msg.content
-    }
-  }
-})
-
-client.login(clientInfo.token)
+  client.login(config.token)
+})()
